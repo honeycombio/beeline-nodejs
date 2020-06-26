4773f5a83bf528ebe53d07db84ccf7eb7f9b3cb6
Katie Poe Bayes <katie@honeycomb.io>
2020-06-26 15:54:10 -0400

    remove build dir, moving to another branch

--- build/src/lib/propagation/typescript/w3c_context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.TRACE_PARENT_HEADER = exports.parseOtelTrace = void 0;
-var core_1 = require("@opentelemetry/core");
-exports.TRACE_PARENT_HEADER = core_1.TRACE_PARENT_HEADER;
-function parseOtelTrace(header) {
-    return core_1.parseTraceParent(header);
-}
-exports.parseOtelTrace = parseOtelTrace;
